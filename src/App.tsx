8a5e3509776dcd571ab694da8c12e7304c913d06
Lucas Barcelos <lucasbarcelos58@gmail.com>
2020-04-24 00:55:12 -0300

    create: manipulate global state with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import usePersistedState from "./utils/usePersistedState";
 
 import GlobalStyle from "./styles/global";
+
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+
 import Header from "./components/Header/index";
 
 function App() {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>("theme", light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
+
   return (
-    <div className="App">
-      <GlobalStyle />
-      <Header />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
   );
 }
 
